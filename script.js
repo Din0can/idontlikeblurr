9f28a9f62e55cbefdd5200d16dba91523388bae4
Dino <alisercoli@gmail.com>
2022-11-09 16:32:12 +0100

    Create Button Logic

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.getElementById('fname').value
 
+const button = document.querySelector('input');
+const paragraph = document.querySelector('p');
+
+button.addEventListener('click', updatefile);
+
+function updatefile() {
+    if (button.value === 'Start machine') {
+        button.value = 'Stop machine';
+        paragraph.textContent = 'The machine has started!';
+    } else {
+        button.value = 'Start machine';
+        paragraph.textContent = 'The machine is stopped.';
+    }
+}
